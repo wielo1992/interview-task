61e8483527b2029064ccaecb5117c189c7b84d3c
Kacper Czerwinski <czerwin92@gmail.com>
2021-12-02 11:03:44 +0100

    fixed mistake with services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,8 +14,6 @@ import { SingleCountryComponent } from './components/single-country/single-count
 import { FooterComponent } from './components/wrapper/footer/footer.component';
 import { HeaderComponent } from './components/wrapper/header/header.component';
 import { WrapperComponent } from './components/wrapper/wrapper.component';
-import { ApiServiceComponent } from './services/api-service/api-service.component';
-import { LogicServiceComponent } from './services/logic-service/logic-service.component';
 
 @NgModule({
   declarations: [
@@ -26,8 +24,6 @@ import { LogicServiceComponent } from './services/logic-service/logic-service.co
     ContinentsComponent,
     CountryListComponent,
     SingleCountryComponent,
-    ApiServiceComponent,
-    LogicServiceComponent,
   ],
   imports: [
     MatIconModule,
