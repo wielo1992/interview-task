268802a3d46b097a4f0c1baf133864da9d82ee8a
Kacper Czerwinski <czerwin92@gmail.com>
2021-12-02 10:12:59 +0100

    bootstrap and angular-materials installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
+import { MatSliderModule } from '@angular/material/slider';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
-import { WrapperComponent } from './components/wrapper/wrapper.component';
-import { HeaderComponent } from './components/wrapper/header/header.component';
-import { FooterComponent } from './components/wrapper/footer/footer.component';
 import { ContinentsComponent } from './components/continents/continents.component';
 import { CountryListComponent } from './components/country-list/country-list.component';
 import { SingleCountryComponent } from './components/single-country/single-country.component';
+import { FooterComponent } from './components/wrapper/footer/footer.component';
+import { HeaderComponent } from './components/wrapper/header/header.component';
+import { WrapperComponent } from './components/wrapper/wrapper.component';
 import { ApiServiceComponent } from './services/api-service/api-service.component';
 import { LogicServiceComponent } from './services/logic-service/logic-service.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -22,13 +23,10 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     CountryListComponent,
     SingleCountryComponent,
     ApiServiceComponent,
-    LogicServiceComponent
+    LogicServiceComponent,
   ],
-  imports: [
-    BrowserModule,
-    NgbModule
-  ],
+  imports: [MatSliderModule, BrowserModule, NgbModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
