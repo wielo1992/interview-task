8a5d8ff548530656e49419b89680d94b9e2c474d
Kacper Czerwinski <czerwin92@gmail.com>
2021-12-02 13:52:51 +0100

    countries by regieon fetched

--- src/app/components/country-list/country-list.component.ts
+++ src/app/components/country-list/country-list.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiServiceService } from 'src/app/services/api-service.service';
 
 @Component({
   selector: 'app-country-list',
   templateUrl: './country-list.component.html',
-  styleUrls: ['./country-list.component.scss']
+  styleUrls: ['./country-list.component.scss'],
 })
 export class CountryListComponent implements OnInit {
+  constructor(private readonly api: ApiServiceService) {}
 
-  constructor() { }
+  readonly countryList$ = this.api.contryList$;
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
